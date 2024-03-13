aac2d3661f1e23b1a8a509c9bca33494310a3e0f
Sean-Cardeno <136882012+Sean-Cardeno@users.noreply.github.com>
2024-03-13 22:23:03 +0800

    Added

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function goToCalendar() {
-    window.location.href = "../CCAPDEVMCO1/Calendar/Calendar.html";
+  window.location.href = "../CCAPDEVMCO1/Calendar/Calendar.html";
+}
+
+
+function openModal() {
+  document.getElementById('profileModal').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('profileModal').style.display = 'none';
+}
+window.onclick = function (event) {
+  let modal = document.getElementById('profileModal');
+  if (event.target == modal) {
+    closeModal();
   }
+}
