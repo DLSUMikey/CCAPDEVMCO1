a5b615fef454ea29e46ff3f119b36c6cbc016fea
Vraiz <82005765+Vraiz@users.noreply.github.com>
2024-03-14 03:08:12 +0800

    Add files via upload

--- character.js
+++ character.js
@@ -237,6 +237,7 @@ window.onload = async function() {
             $(".owned").css("display", "none")
             switched = false
             profileLink = "FindAcc.html"//change to Profile.html
+            loadItems()//IMPORTANT
             updateCurrency()
         } else {
             console.log("Error")
